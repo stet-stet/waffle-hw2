6db704dc6df0ef1a9c723ebd9489666f5909f891
stet_stet <jayeonyi@snu.ac.kr>
2018-11-09 23:15:30 +0900

    the site is complete. We should write tests and run

--- pseudoDC/board/urls.py
+++ pseudoDC/board/urls.py
@@ -3,16 +3,20 @@ from django.urls import path
 from . import views
 app_name = 'board'
 urlpatterns = [
+    # ##########################Views##########################
+    path('nope/', views.four_oh_four, name='fourOhFour'),
     path('', views.Index, name='index'),
-    path('write/', views.Write, name='write'),
     path('read/<int:post_id>', views.Read, name='read'),
-    path('update/<int:post_id>', views.Update, name='update'),
+    path('write/', views.Write, name='write'),
+    path('verify/<int:post_id>/', views.Verify, name='verify'),
+    path('update/<int:post_id>/<str:password>/', views.Update, name='update'),
     path('delete/<int:post_id>', views.Delete, name='delete'),
+    # ##########################Queries########################
+    path('write/render/', views.PostBlogpostQuery, name='postblogpostquery'),
+    path('update/get/<int:post_id>',
+         views.HandleVerifyQuery, name="handleverifyquery"),
     path('delete/action/<int:post_id>',
          views.HandleDeleteQuery, name="handledeletequery"),
     path('update/action/<int:post_id>',
-         views.HandleUpdateQuery, name="handleupdatequery"),
-    path('nope/', views.four_oh_four, name='fourOhFour'),
-    path('write/render/', views.PostBlogpostQuery, name='postblogpostquery')
-    # delete is to be implemented separately.
+         views.HandleUpdateQuery, name="handleupdatequery")
 ]
--- pseudoDC/board/views.py
+++ pseudoDC/board/views.py
@@ -29,9 +29,14 @@ def Write(http_request):
     return render(http_request, 'board/write.html', {})
 
 
-def Update(http_request, post_id):
+def Verify(http_request, post_id):
     B = get_object_or_404(BlogPost, pk=post_id)
-    if B.private is True:
+    return render(http_request, 'board/verify.html', {"blogpost": B})
+
+
+def Update(http_request, post_id, password):
+    B = get_object_or_404(BlogPost, pk=post_id)
+    if B.password != password:
         return four_oh_four(http_request)
     else:
         return render(http_request, 'board/modify.html', {"blogpost": B})
@@ -45,7 +50,7 @@ def Delete(http_request, post_id):
 
 
 def PostBlogpostQuery(http_request):
-    B = Blogpost(
+    B = BlogPost(
         subject=http_request.POST['subject'],
         text=http_request.POST['text'],
         password=http_request.POST['password'],
@@ -56,6 +61,14 @@ def PostBlogpostQuery(http_request):
     )
     B.save()
     return HttpResponseRedirect(reverse('board:read', args=(B.pk,)))
+
+
+def HandleVerifyQuery(http_request, post_id):
+    B = get_object_or_404(BlogPost, pk=post_id)
+    if http_request.POST['password'] == B.password:
+        return HttpResponseRedirect(reverse('board:update', args=(post_id, B.password)))
+    else:
+        return HttpResponseRedirect(reverse('board:index'))
 
 
 def HandleUpdateQuery(http_request, post_id):
@@ -71,6 +84,7 @@ def HandleUpdateQuery(http_request, post_id):
 
 def HandleDeleteQuery(http_request, post_id):
     B = get_object_or_404(BlogPost, pk=post_id)
-    B.private = True
+    if http_request.POST['password'] == B.password:
+        B.private = True
     B.save()
     return HttpResponseRedirect(reverse('board:index'))
